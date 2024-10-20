1273cf77a9be610e69084976b57671633658e70d
hbashar434 <h.bashar434@gmail.com>
2024-10-20 12:06:21 +0600

    add api fetching

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,17 @@
-  
+const API_URL = "https://gutendex.com/books";
+
+export const fetchBooks = async (page = 1, titleFilter = "") => {
+  try {
+    const response = await fetch(
+      `${API_URL}?page=${page}&search=${titleFilter}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch books:", error);
+    return { results: [], total_pages: 0 };
+  }
+};
